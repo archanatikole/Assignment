c85b364d9e8f82ed9bc903aa4072b66fd3571c7d
Archana Tikole <archanatikole@gmail.com>
2020-07-17 20:15:58 +0530

    Initiated new project

---
+++ testLeadAssignment/src/main/java/com/restassured/testLeadAssignment/App.java
@@ -0,0 +1,13 @@
+package com.restassured.testLeadAssignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
